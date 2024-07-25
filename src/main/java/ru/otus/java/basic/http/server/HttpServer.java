279dcb14e37b1a172bcd00ee7b38785fb7a2529f
lsdvitamin <lsdvitamin7@gmail.com>
2024-07-25 13:48:34 +0300

    Внес изменения с ДЗ 23 в lesson2

--- src/main/java/ru/otus/java/basic/http/server/HttpServer.java
+++ src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -1,13 +1,13 @@
 package ru.otus.java.basic.http.server;
 
-import ru.otus.java.basic.http.server.processors.AnotherHelloWorldRequestProcessor;
-import ru.otus.java.basic.http.server.processors.HelloWorldRequestProcessor;
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
+    static ExecutorService executorService = Executors.newCachedThreadPool();
     private int port;
     private Dispatcher dispatcher;
 
@@ -20,20 +20,27 @@ public class HttpServer {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
+                Socket socket = serverSocket.accept();
+                executorService.execute(() -> {
                     byte[] buffer = new byte[8192];
-                    int n = socket.getInputStream().read(buffer);
-                    if (n < 1) {
-                        continue;
+                    int n = 0;
+                    try {
+                        n = socket.getInputStream().read(buffer);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
                     }
                     String rawRequest = new String(buffer, 0, n);
                     HttpRequest request = new HttpRequest(rawRequest);
                     request.printInfo(true);
-                    dispatcher.execute(request, socket.getOutputStream());
-                }
+                    try {
+                        dispatcher.execute(request, socket.getOutputStream());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
